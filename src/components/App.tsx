d2eb34be5ea247a08a43ee769f8ad9049c686b0d
Alexandr Siryj <theobroma222@gmail.com>
2019-11-12 21:17:00 +0200

    add styles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,18 +2,18 @@ import * as React from "react";
 import { hot } from "react-hot-loader";
 
 const reactLogo = require("./../assets/img/react_logo.svg");
-import "./../assets/scss/App.scss";
+import "./../assets/scss/index.scss";
 
 class App extends React.Component<{}, undefined> {
-    public render() {
-        return (
-            <div className="app">
-                <h1>Hello World!</h1>
-                <p>Foo to the barz</p>
-                <img src={reactLogo} height="480"/>
-            </div>
-        );
-    }
+  public render() {
+    return (
+      <div className="app">
+        <h1>Hello World!</h1>
+        <p>Foo to the barz</p>
+        <img src={reactLogo} height="480" />
+      </div>
+    );
+  }
 }
 
 declare let module: object;
