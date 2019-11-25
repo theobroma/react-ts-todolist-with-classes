11cf3ad683edc7b388638a30c9dd23e31b91e33d
Alexandr Siryj <theobroma222@gmail.com>
2019-11-25 04:35:54 +0200

    add interfaces

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,55 +1,41 @@
 import * as React from 'react';
-import * as cx from 'classnames';
-//mock data if needed
-// import { Todo, IState } from '../model';
-// import TodoTextInput from './TodoTextInput';
+// import * as cx from 'classnames';
 
-// interface TodoItemProps {
-//   todo: Todo;
-//   key?: any;
-// }
+import { ITodoItem } from '../interfaces';
 
-// interface TodoItemState {
-//   completed: boolean;
-// }
+const TodoItem = (props: ITodoItem) => {
+  const {
+    todo: { _id, text, completed },
+  } = props;
 
-export class TodoItem extends React.Component<any, any> {
-  render() {
-    const {
-      todo: { completed, text, _id },
-    } = this.props;
-    console.log(this.props);
-
-    let element;
-    element = (
-      <div className="view">
-        <input
-          className="toggle"
-          type="checkbox"
-          checked={completed}
-          // onChange={() => toggleTodo(_id, completed, text)}
-        />
-        <label
-          htmlFor="itself"
-          //  onDoubleClick={this.handleDoubleClick}
-        >
-          {text}
-        </label>
-        <button
-          className="destroy"
-          // onClick={() => removeTodo(_id)}
-        />
-      </div>
-    );
-
-    return (
-      <li
-      // className={cx({ completed: completed, editing: this.state.editing })}
+  let element;
+  element = (
+    <div className="view">
+      <input
+        className="toggle"
+        type="checkbox"
+        checked={completed}
+        // onChange={() => toggleTodo(_id, completed, text)}
+      />
+      <label
+        htmlFor="itself"
+        //  onDoubleClick={this.handleDoubleClick}
       >
-        {element}
-      </li>
-    );
-  }
-}
+        {text}
+      </label>
+      <button
+        className="destroy"
+        // onClick={() => removeTodo(_id)}
+      />
+    </div>
+  );
+  return (
+    <li
+    // className={cx({ completed: completed, editing: this.state.editing })}
+    >
+      {element}
+    </li>
+  );
+};
 
 export default TodoItem;
---
+++ src/interfaces.ts
@@ -0,0 +1,30 @@
+// Todo interface
+export interface ITodo {
+  _id: string;
+  text: string;
+  completed: boolean;
+}
+
+// Todo form interface
+export interface ITodoForm {
+  todos: ITodo[];
+  // handleTodoCreate: (todo: ITodo) => void;
+}
+
+// Todo list interface
+export interface ITodoList {
+  // handleTodoUpdate: (event: React.ChangeEvent<HTMLInputElement>, id: string) => void;
+  // handleTodoRemove: (id: string) => void;
+  // handleTodoComplete: (id: string) => void;
+  // handleTodoBlur: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  todos: ITodo[];
+}
+
+// Todo item interface
+export interface ITodoItem {
+  // handleTodoUpdate: (event: React.ChangeEvent<HTMLInputElement>, id: string) => void;
+  // handleTodoRemove: (id: string) => void;
+  // handleTodoComplete: (id: string) => void;
+  // handleTodoBlur: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  todo: ITodo;
+}
